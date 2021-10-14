9ab51a7a1025d49318ca7d53b091fb5c4e179f62
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-15 01:29:58 +0200

    02.1.Variables

---
+++ 02.Basics/1- Variables.js
@@ -0,0 +1,15 @@
+/*
+Cannot be a reserved keyword
+Should be meaningful
+Cannot start with a number
+Cannot contain a space or hyphen(-)
+Are case-sensitive
+*/
+
+// declaring a variable without type
+let name_undefined;
+console.log(name_undefined); // == undefined
+
+let firstName = "kareem";   
+let lastName = "azam";
+console.log(firstName, lastName);
