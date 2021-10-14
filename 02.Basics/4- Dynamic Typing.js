127b2413503ee6dbf3dccae56161acfa23e02660
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-15 01:57:32 +0200

    02.4.Dynamic Typing

---
+++ 02.Basics/4- Dynamic Typing.js
@@ -0,0 +1,22 @@
+/** tow types of programming language :
+ *  Static language
+ *  Dynamic language
+ * javascript is a dynamic language
+ */
+
+let name = "kareem"; // String Literal
+console.log(typeof name); // String
+name = 1;
+console.log(typeof name); // Number
+
+let age = 30; // Number Literal
+console.log(typeof age); // Number
+
+let isApproved = true; // Boolean Literal
+console.log(typeof isApproved); // Boolean
+
+let firstName = undefined; // default if no assignment to variable
+console.log(typeof firstName); // undefined
+
+let lastName = null; // to clear a value of a variable
+console.log(typeof lastName); // Object
