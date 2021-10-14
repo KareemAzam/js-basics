10ee2ef04ad5c85c8fdfae54fd050c2a86baf7a4
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-15 01:44:19 +0200

    02.3.Primitive Types - Title

---
+++ 02.Basics/3- Primitive Types - Title.js
@@ -0,0 +1,13 @@
+/** Primitive Types :
+ * 1. String
+ * 2. Number
+ * 3. Boolean
+ * 4. undefined
+ * 5. null
+ */
+
+let name = "kareem"; // String Literal
+let age = 30; // Number Literal
+let isApproved = true; // Boolean Literal
+let firstName = undefined; // default if no assignment to variable
+let lastName = null; // to clear a value of a variable
