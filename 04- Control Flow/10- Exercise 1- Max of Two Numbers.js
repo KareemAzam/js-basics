fcc37c32d09281cf2692613b3a077cc5cfa652d7
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-16 01:02:25 +0200

    04.10.Exercise 1- Max of Two Numbers

---
+++ 04- Control Flow/10- Exercise 1- Max of Two Numbers.js
@@ -0,0 +1,11 @@
+/**Max of Two Numbers
+ * Write a function that take two number
+ * and return the maximum of the two
+ */
+
+function Max(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+let number = Max(5, 10);
+console.log(number);
