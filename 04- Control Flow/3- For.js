fa316b99c65dafd3b73df0d4b77f8763a35124f3
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-15 20:09:07 +0200

    04.3.For

---
+++ 04- Control Flow/3- For.js
@@ -0,0 +1,16 @@
+/** For
+ * Definition
+ *  For(initialExpression; Condition;incrementExpression)
+ *
+ * for (let i = 0; i < 5; )
+ * {statement}
+ */
+
+for (let i = 0; i < 5; i++) {
+  console.log("hello World", i);
+}
+
+// display the odd number between 1 to 5
+for (let i = 0; i <= 5; i++) {
+  if (i % 2 !== 0) console.log(i);
+}
