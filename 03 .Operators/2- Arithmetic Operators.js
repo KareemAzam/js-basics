15f506c7ac5c2580aa99e53dee594137b5aac1c0
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-15 03:47:47 +0200

    03.2.Arithmetic Operators

---
+++ 03 .Operators/2- Arithmetic Operators.js
@@ -0,0 +1,27 @@
+/**Arithmetic
+ * Addition +
+ * Subtraction -
+ * Multiplication *
+ * Division /
+ * Remainder of division %
+ * Exponent ** الاس التربيعى
+ * Increment ++
+ * Decrement --
+ */
+
+let x = 10;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+//Increment ++
+console.log(++x); //  increase by 1 then show x  = 11
+console.log(x++); // show x first then increase by 1 = 10
+//Decrement --
+console.log(--x); //  decrease by 1 then show x  = 9
+console.log(x--); // show x first then decrease by 1 = 10
