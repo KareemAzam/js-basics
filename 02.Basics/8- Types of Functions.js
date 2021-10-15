de7802247d39b796e26bf8fffc59c3ab9c408bf9
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-15 03:01:28 +0200

    02.8.Types of Functions

---
+++ 02.Basics/8- Types of Functions.js
@@ -0,0 +1,22 @@
+/** Reference Types :
+ * 1. Object
+ * 2. Array
+ * 3. Function
+ */
+
+/** 3. Function Types
+ * Performing a task : void
+ * calculating a value : return value
+ */
+
+// * Performing a task
+function great(name, lastName) {
+  console.log("Hello" + " " + name + " " + lastName);
+}
+
+// * calculating a value
+function square(number) {
+  return number * number;
+}
+
+console.log(square(2));
