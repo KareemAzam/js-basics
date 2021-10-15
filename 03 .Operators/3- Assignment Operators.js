994387a146a74e30e4143a09003742f1e31b5549
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-15 03:52:27 +0200

    03.3.Assignment Operators

---
+++ 03 .Operators/3- Assignment Operators.js
@@ -0,0 +1,15 @@
+/**Assignment
+ * Assignment operators are operators that assign a value to a variable
+ *  Addition assignment +=
+ *  Subtraction assignment -=
+ *  Multiplication assignment *=
+ *  Division assignment /=
+ */
+
+let x = 10;
+
+x = x + 5;
+x += 5;
+
+x = x * 3;
+x *= 3;
