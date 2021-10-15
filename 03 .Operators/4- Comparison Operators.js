f8320d1c9e7b5a4419993b4dc927f9e7d11def77
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-15 04:03:42 +0200

    03.4.Comparison Operators

---
+++ 03 .Operators/4- Comparison Operators.js
@@ -0,0 +1,21 @@
+/**Comparison
+ * to run true/false tests
+ *  === Strict equality
+ *  !== Strict-non-equality
+ *  <   Less than
+ *  >   Greater than
+ *  <=  Less than or equal to
+ *  >=  Greater than or equal to
+ */
+
+let x = 1;
+
+// Relational
+console.log(x > 0);
+console.log(x >= 1);
+console.log(x < 1);
+console.log(x <= 1);
+
+// Equality
+console.log(x === 1);
+console.log(x !== 1);
