2d5304fc5a285f1d1eb8416fb7412dad0890ea83
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-15 21:00:09 +0200

    04.8.For...of

---
+++ 04- Control Flow/8- For...of.js
@@ -0,0 +1,11 @@
+/** for of
+ * new way to iterate over array elements
+ * Definition
+ * for(let element of elements)
+ */
+
+// iterate over array elements
+const colors = ["red", "green", "blue"];
+for (let color of colors) {
+  console.log(color);
+}
