38825920b4f6661386cc470a0e894be3b23a2c5c
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-16 18:06:43 +0200

    04.11.Exercise 2- Landscape or Portrait

---
+++ 04- Control Flow/11- Exercise 2- Landscape or Portrait.js
@@ -0,0 +1,11 @@
+/**Landscape or Portrait
+ * write a function that implement this function
+ * isLandscape(width,height)
+ * return true if the image is landscape
+ * wish mean width > height
+ * return false otherwise
+ */
+console.log(isLandscape(800, 600));
+function isLandscape(width, height) {
+  return width > height;
+}
