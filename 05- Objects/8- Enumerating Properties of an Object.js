a6feb8ea5a590b9bedd3191215de248abb6bb532
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-19 03:39:58 +0200

    05.8.Enumerating Properties of an Object

---
+++ 05- Objects/8- Enumerating Properties of an Object.js
@@ -0,0 +1,37 @@
+/**
+ ** (let key of object)
+ * for_of only use with i
+ *! for (let key of circle) {
+ *! console.log(key, circle[key]);
+ *! }
+ *! error=> circle is not iterable
+ */
+
+// Constructor Function
+const circle = {
+  radius: 1,
+  draw() {
+    console.log('draw');
+  },
+};
+
+for (let key in circle) {
+  console.log(key, circle[key]);
+}
+
+//* Object.keys(obj) :
+//  Returns string[] the names of the enumerable string properties and methods of an object.
+for (let key of Object.keys(circle)) {
+  console.log(key, circle[key]);
+}
+
+//* Object.entries(obj) :
+// Returns an array of key/values of the enumerable properties of an object.
+for (let entry of Object.entries(circle)) {
+  console.log(entry);
+}
+
+// check if property exists
+if ('radius' in circle) {
+  console.log('yes');
+}
