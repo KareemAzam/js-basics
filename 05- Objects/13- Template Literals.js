33ee228ee221d6fc6a354b57247f7c77856f7c70
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-19 20:45:17 +0200

    05.13.Template Literals

---
+++ 05- Objects/13- Template Literals.js
@@ -0,0 +1,13 @@
+const name = 'kareem';
+const message = 'hi ' + name + ',/n';
+
+console.log(message);
+
+const anther = `hi ${name} ${2 + 3}
+
+Thank you for joining my mailing list
+
+Regards,
+kareem
+`;
+console.log(anther);
