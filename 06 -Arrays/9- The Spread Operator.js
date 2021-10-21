7b25d9a46138de1442f9972d078c5fd1c9fcd26f
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-21 05:27:56 +0200

    09.9.The Spread Operator

---
+++ 06 -Arrays/9- The Spread Operator.js
@@ -0,0 +1,7 @@
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+// const combined = first.concat(second);
+const combined = [...first, ...second];
+// const slice = combined.slice(2;
+const copy = [...combined];
