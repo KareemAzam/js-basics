6849c106f5267eedbc619500eb90e072ab294064
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-21 08:26:52 +0200

    06.14.Filtering an Array

---
+++ 06 -Arrays/14- Filtering an Array.js
@@ -0,0 +1,8 @@
+/**Filtering an Array
+ *return new array when expression === true
+ */
+
+const numbers = [1, 2, -1, 4, 5];
+
+const filtered = numbers.filter((n) => n >= 0);
+console.log(filtered);
