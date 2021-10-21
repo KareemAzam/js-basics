4459d471321951b9555f0c41b52124e7124a7e7b
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-21 05:48:36 +0200

    06.11.Joining Arrays

---
+++ 06 -Arrays/11- Joining Arrays.js
@@ -0,0 +1,17 @@
+/** Joining Arrays
+ *
+ */
+
+//Joining Arrays
+const numbers = [1, 2, 3, 4];
+const joined = numbers.join(',');
+console.log(joined);
+
+//split Arrays
+const message = 'this is my first message';
+const part = message.split(' ');
+console.log(part);
+
+
+const combined = part.join('-');
+console.log(combined);
