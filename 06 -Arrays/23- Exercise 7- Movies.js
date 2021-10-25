03da9ef90a28c04038d8c549c6ef05bbc1fc2701
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-10-25 03:42:46 +0200

    06.23.Exercise 7- Movies

---
+++ 06 -Arrays/23- Exercise 7- Movies.js
@@ -0,0 +1,22 @@
+/**Movies
+ * return the array of movies object based on this criteria
+ ** All the movies in 2018 with rating > 4
+ ** Sort them by there rating
+ ** descending order
+ ** pick there title
+ */
+const movies = [
+  { title: 'a', year: 2018, rating: 4.5 },
+  { title: 'b', year: 2018, rating: 4.7 },
+  { title: 'c', year: 2018, rating: 3 },
+  { title: 'd', year: 2017, rating: 4.5 },
+];
+
+// the solution
+const title = movies
+  .filter((m) => m.year === 2018 && m.rating >= 4)
+  .sort((a, b) => a.rating - b.rating)
+  .reverse()
+  .map((m) => m.title);
+
+console.log(title);
